c64891ae38c58e44886ea786739ca01b855d52dc
Zurab Zurabishvili <accs.zukazurabishvili@gmail.com>
2023-10-07 19:02:06 +0400

    add custom color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
-// tailwind.config.js
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        space: {
+          lightyellow: "#FFD60A",
+          yellow: "#FFC300",
+          blue: "#003566",
+          darkblue: "#001D3D",
+          black: "#000814",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
