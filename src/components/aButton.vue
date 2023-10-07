4212b39cbe2ca28338080701cf0f09dd8679b96f
Zurab Zurabishvili <accs.zukazurabishvili@gmail.com>
2023-10-07 21:43:23 +0400

    add button and loading component

---
+++ src/components/aButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <button
+    class="p-5 border-2 border-space-darkblue rounded-2xl bg-opacity-0 text-space-yellow hover:bg-opacity-100 hover:bg-space-blue hover:text-space-lightyellow transition duration-500 ease-in-out"
+  >
+    {{ buttonText }}
+  </button>
+</template>
+
+<script setup>
+defineProps({
+  buttonText: String,
+});
+</script>
---
+++ src/components/aLoading.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex flex-col items-center justify-center h-screen">
+    <h5 class="text-2xl font-bold text-space-lightyellow animate-ping">
+      Loading ...
+    </h5>
+    <img src="../assets/rocket-svgrepo-com.svg" class="m-5 w-20 rocket" />
+    <h4 class="text-2xl font-bold text-space-lightyellow animate-bounce">
+      {{ randText() }}
+    </h4>
+  </div>
+</template>
+
+<script setup>
+const randText = () => {
+  const texts = ["fact1", "fact2", "fact3", "fact4"];
+  return texts[(Math.random() * texts.length) | 0];
+};
+</script>
+
+<style>
+@keyframes move-rotate {
+  0% {
+    transform: translate(-30px, -10px) rotate(45deg);
+  }
+  100% {
+    transform: translate(30px, 10px) rotate(45deg);
+  }
+}
+
+.rocket {
+  animation: move-rotate 5s alternate infinite;
+}
+</style>
